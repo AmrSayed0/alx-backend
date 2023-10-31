1f525872ee7277e9068fac3da779f2ceca3231e9
AmrSayed0 <amracademycode@gmail.com>
2023-10-31 15:47:12 +0200

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
+"""Parametrize templates
 """
 from flask_babel import Babel
 from flask import Flask, render_template, request
